9355780129da0d3568b6a37d4b788d7319cd9da8
jeekc0308 <jeekc0308@gmail.com>
2020-04-10 15:51:40 +0900

    Update Portfolio: Add Space

--- src/components/Chapters/PortfolioChapter/list.ts
+++ src/components/Chapters/PortfolioChapter/list.ts
@@ -12,78 +12,86 @@ import job1 from "../../../assets/image/portfolio/2019job_1.jpg";
 import job2 from "../../../assets/image/portfolio/2019job_2.jpg";
 import job3 from "../../../assets/image/portfolio/2019job_3.jpg";
 import job4 from "../../../assets/image/portfolio/2019job_4.jpg";
-
+import space from "../../../assets/image/portfolio/space.png";
 const portfolioList = [
   {
     name: "circles.",
     members: "김현우, 표영우, 김태양",
     prize: "디지털콘텐츠 개발대회 대상",
     description: "빠르고 편리한 통합 동아리 매니지먼트 솔루션",
-    images: [circles1, circles2]
+    images: [circles1, circles2],
   },
   {
     name: "아띠 (Atti)",
     members: "김현우",
     prize: "모바일콘텐츠 개발대회 은상",
     description: "외국인 관광객을 위한 관광 정보앱",
-    images: [Atti]
+    images: [Atti],
   },
+  {
+    name: "스페이스 (Space)",
+    members: "김도영, 김민, 정찬효",
+    prize: "디지털콘텐츠 개발대회 동상",
+    description: "빠르고 편리한 공유경제 마켓 플랫폼",
+    images: [space],
+  },
+
   {
     name: "빌더즈",
     members: "김도영, 신나영, 정찬효, 황두영",
     prize: "모바일콘텐츠 개발대회 동상",
     description: "대회, 공모전 출전을 위한 팀 빌딩 앱",
-    images: [builders]
+    images: [builders],
   },
   {
     name: "픽토체인",
     members: "정찬효",
     prize: "선린해커톤 금상",
     description: "현실 자물쇠 키체인 서비스",
-    images: [piktochain]
+    images: [piktochain],
   },
   {
     name: "한울",
     members: "김민",
     prize: "선린해커톤 은상",
     description: "다이어리를 통한 진로 분석, 추천 서비스",
-    images: [hanul]
+    images: [hanul],
   },
   {
     name: "StuFlo",
     members: "김현우, 표영우",
     prize: "선린해커톤 동상",
     description: "흐름과 플로우를 통한 학습 서비스",
-    images: [stuflo]
+    images: [stuflo],
   },
   {
     name: "FlexiLearn",
     members: "표영우",
     prize: "e-ICON 세계대회 '3rd Place Award' (APEC국제교육협력원장상)",
     description: "누구에게나 열려있는 통합교육 플랫폼",
-    images: [flexilearn]
+    images: [flexilearn],
   },
   {
     name: "ForestKeeper",
     members: "김도영, 김현우, 송영범",
     prize: "IoT 아이디어 공모전 금상",
     description: "불법 벌목, 산불 감지를 위한 IoT 장비 및 서비스",
-    images: [forestKeeper]
+    images: [forestKeeper],
   },
   {
     name: "위험감지 알림가방",
     members: "이현재",
     prize: "WICO (세계발명창의대회) 금상",
     description: "위험을 감지해 방지하는 가방",
-    images: [bag]
+    images: [bag],
   },
   {
     name: "2019 고졸성공 취업대박람회",
     members: "김현우, 이정우, 정민서, 정찬효, 표영우",
     prize: "동아리 경진대회 특별상",
     description: "IoT 장비를 이용한 체험 프로그램 운영 및 앱 시연",
-    images: [job1, job2, job3, job4]
-  }
+    images: [job1, job2, job3, job4],
+  },
 ];
 
 export default portfolioList;
