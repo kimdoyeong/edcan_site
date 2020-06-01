5709545864ff1fa436079384b6dadbb1d602b2dc
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-06-01 11:12:18 +0900

    Update Button

--- src/components/Chapters/MainChapter.tsx
+++ src/components/Chapters/MainChapter.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { css } from "styled-components";
+import { MdPlayArrow } from "react-icons/md";
 import Chapter from "../Chapter";
 import edcanLogo from "../../assets/logo/edcan.svg";
-import { css } from "styled-components";
 import darkMode from "../../utils/style/darkMode";
 import { AButton } from "../UI/Button";
 
@@ -23,8 +24,17 @@ const style = css`
   .slogan {
     color: #575757;
   }
-  .apply {
+  .buttons {
     margin-top: 1em;
+    display: flex;
+    & > * {
+      margin-left: 0.5em;
+      margin-right: 0.5em;
+    }
+    & > .apply {
+      background: rgb(20, 167, 198);
+      color: white;
+    }
   }
 
   ${darkMode(css`
@@ -44,8 +54,13 @@ function MainChapter() {
       <img src={edcanLogo} alt="EDCAN Logo" className="logo" />
       <h1 className="title">EDCAN</h1>
       <p className="slogan">We create new mobile century on the planet.</p>
-      <div className="apply">
-        <AButton href="https://www.facebook.com/edcancircle">지원하기</AButton>
+      <div className="buttons">
+        <AButton href="https://www.facebook.com/edcancircle" className="apply">
+          지원하기
+        </AButton>
+        <AButton href="https://www.youtube.com/watch?v=91hmkywn4DQ&feature=youtu.be">
+          <MdPlayArrow /> 홍보영상 보기
+        </AButton>
       </div>
     </Chapter>
   );
